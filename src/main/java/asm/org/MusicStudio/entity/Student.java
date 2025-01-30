d538accf93ceb3895ad424b94fe9c9b9ef7b19bc
Khemiri-esprit <MohamedZied.Khemiri@esprit.tn>
2025-01-31 00:36:07 +0100

    Migrate to JavaFX and implement main application UI
    - Added JavaFX dependencies and maven plugin
    - Refactored MusicStudioApplication to use JavaFX Application framework
    - Created MainController with user management functionality
    - Updated User and derived classes to use JavaFX properties
    - Added MainView.fxml for application layout
    - Included basic CSS styling for the application

--- src/main/java/asm/org/MusicStudio/entity/Student.java
+++ src/main/java/asm/org/MusicStudio/entity/Student.java
@@ -1,26 +1,17 @@
 package asm.org.MusicStudio.entity;
     
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import lombok.Builder;
-import lombok.AllArgsConstructor;
-
 import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
-@Data
-@EqualsAndHashCode(callSuper = true)
-@NoArgsConstructor
-@SuperBuilder
 public class Student extends User {
-    @Builder.Default
     private Set<Course> courses = new HashSet<>();
-    @Builder.Default
     private List<Enrollment> enrollments = new ArrayList<>();
+
+    public Student() {
+        super();
+    }
 
     public Student(Integer id, String name, String email, String role) {
         super(id, name, email, Role.valueOf(role.toUpperCase()));
--- src/main/java/asm/org/MusicStudio/entity/Teacher.java
+++ src/main/java/asm/org/MusicStudio/entity/Teacher.java
@@ -1,23 +1,14 @@
 package asm.org.MusicStudio.entity;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
-@EqualsAndHashCode(callSuper = true)
-@SuperBuilder
-@NoArgsConstructor
-@AllArgsConstructor
 public class Teacher extends User {
-    @Builder.Default
     private List<Course> assignedCourses = new ArrayList<>();
+
+    public Teacher() {
+        super();
+    }
 
     public Teacher(Integer id, String name, String email, String role) {
         super(id, name, email, Role.valueOf(role.toUpperCase()));
